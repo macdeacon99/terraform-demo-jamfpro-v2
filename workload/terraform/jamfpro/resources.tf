f8cf1fcf47a7b0ab420559418840b262c95d40ba
Joseph Little <joelittle11@live.com>
2024-10-29 16:38:43 +0000

    amended buildings

--- workload/terraform/jamfpro/resources.tf
+++ workload/terraform/jamfpro/resources.tf
@@ -1,26 +1,7 @@
 
-resource "jamfpro_building" "jamfpro_building_001" {
-  name            = "tf-demo-v2-LONDON WALL"
-  street_address1 = "The McIntosh Tree"
-  street_address2 = "One Apple Park Way"
-  city            = "Cupertino"
-  state_province  = "California"
-  zip_postal_code = "95014"
-  country         = "The United States of America"
-}
-
-resource "jamfpro_building" "jamfpro_building_002" {
-  name            = "tf-demo-v2-LONDON WALL2"
-  street_address1 = "The McIntosh Tree"
-  street_address2 = "One Apple Park Way"
-  city            = "Cupertino"
-  state_province  = "California"
-  zip_postal_code = "95014"
-  country         = "The United States of America"
-}
 
 resource "jamfpro_building" "jamfpro_building_003" {
-  name            = "tf-demo-v2-LONDON WALL2"
+  name            = "tf-demo-v2-LONDON WALL3"
   street_address1 = "The McIntosh Tree"
   street_address2 = "One Apple Park Way"
   city            = "Cupertino"
