9c193f699755fc817d32d087ad25ece12d8c2450
Joseph Little <joelittle11@live.com>
2024-10-29 16:40:38 +0000

    many buildings

--- workload/terraform/jamfpro/resources.tf
+++ workload/terraform/jamfpro/resources.tf
@@ -1,7 +1,8 @@
 
 
 resource "jamfpro_building" "jamfpro_building_003" {
-  name            = "tf-demo-v2-LONDON WALL3"
+  count = 10
+  name            = "tf-demo-v2-London Wall-${count.index}"
   street_address1 = "The McIntosh Tree"
   street_address2 = "One Apple Park Way"
   city            = "Cupertino"
