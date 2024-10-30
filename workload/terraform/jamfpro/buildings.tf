c589f4667bd6498164e965c8b10c46b31baa51a2
thejoeker12 <62243485+thejoeker12@users.noreply.github.com>
2024-10-30 13:47:26 +0000

    New building

--- workload/terraform/jamfpro/buildings.tf
+++ workload/terraform/jamfpro/buildings.tf
@@ -10,3 +10,8 @@ resource "jamfpro_building" "jamfpro_building_003" {
   zip_postal_code = "95014"
   country         = "The United States of America"
 }
+
+
+resource "jamfpro_building" "jamfpro_building_003" {
+  name            = "tf-demo-v2-Dafydd's House"
+}
