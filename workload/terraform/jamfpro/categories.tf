d837dff7299af03921ca74603cf687a575005c5d
thejoeker12 <62243485+thejoeker12@users.noreply.github.com>
2024-10-30 12:53:07 +0000

    new category

--- workload/terraform/jamfpro/categories.tf
+++ workload/terraform/jamfpro/categories.tf
@@ -5,3 +5,7 @@ resource "jamfpro_category" "category_1" {
 resource "jamfpro_category" "category_2" {
   name = "Eng - JL Demo V2"
 }
+
+resource "jamfpro_category" "category_3" {
+  name = "Marketing - JL Demo V2"
+}
