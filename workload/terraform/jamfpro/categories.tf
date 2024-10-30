5bbe17256907035fada8882290039632556c614e
thejoeker12 <62243485+thejoeker12@users.noreply.github.com>
2024-10-30 15:31:00 +0000

    debug

--- workload/terraform/jamfpro/categories.tf
+++ workload/terraform/jamfpro/categories.tf
@@ -13,7 +13,3 @@ resource "jamfpro_category" "category_3" {
 resource "jamfpro_category" "category_4" {
   name = "Sales - JL Demo V2"
 }
-
-resource "jamfpro_category" "category_5" {
-  name = "Facilities - JL Demo V2"
-}
