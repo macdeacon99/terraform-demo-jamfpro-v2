1b6ca838b08da89f74d832ab8dbea1b77157d085
thejoeker12 <62243485+thejoeker12@users.noreply.github.com>
2024-10-30 11:31:57 +0000

    refactored branch check to python script

---
+++ .github/scripts/branch_name_check.py
@@ -0,0 +1,18 @@
+"""
+This utility collects a branch name string from the environment at BRANCH_NAME
+It will them deem if the prefix is eligiable as per the allowed_branch_prefixes.txt file
+"""
+import os
+
+BRANCH_NAME = os.environ.get("BRANCH_NAME")
+if not BRANCH_NAME:
+    raise EnvironmentError("BRANCH_NAME environment variable is empty")
+
+
+with open("allowed_branch_prefixes.txt", "r", encoding="UTF-8") as f:
+    for l in f.read():
+        if l in BRANCH_NAME:
+            print(f"Branch name is valid: prefix:{l}, name: {BRANCH_NAME}")
+            exit()
+
+raise ValueError(f"branch name prefix is invalid: {BRANCH_NAME}")
