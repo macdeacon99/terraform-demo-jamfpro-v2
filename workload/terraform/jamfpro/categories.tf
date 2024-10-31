d7eb1ed7739235af21d8358a9c1cefa597a84b95
Joseph Little <joelittle11@live.com>
2024-10-31 13:04:18 +0000

    renamed category

--- workload/terraform/jamfpro/categories.tf
+++ workload/terraform/jamfpro/categories.tf
@@ -7,7 +7,7 @@ resource "jamfpro_category" "category_2" {
 }
 
 resource "jamfpro_category" "category_3" {
-  name = "Marketing - JL Demo V2"
+  name = "Marketing-1 - JL Demo V2"
 }
 
 resource "jamfpro_category" "category_4" {
