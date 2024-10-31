19dffe8b1fdfc323a5ad88855c72124facc5f9a5
Joseph Little <joelittle11@live.com>
2024-10-31 13:16:43 +0000

    changed trigger

--- workload/terraform/jamfpro/categories.tf
+++ workload/terraform/jamfpro/categories.tf
@@ -10,6 +10,6 @@ resource "jamfpro_category" "category_3" {
   name = "Marketing-1 - JL Demo V2"
 }
 
-resource "jamfpro_category" "category_4" {
-  name = "Marketing - JL Demo V2 - 2"
-}
+# resource "jamfpro_category" "category_4" {
+#   name = "Marketing - JL Demo V2 - 2"
+# }
