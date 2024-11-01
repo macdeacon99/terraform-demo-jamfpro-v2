a0a99fe6b7bf1c8f933c7ba93e64effa740a8e3a
Joseph Little <joelittle11@live.com>
2024-11-01 13:05:06 +0000

    feat: 6 new buildings

--- workload/terraform/jamfpro/buildings.tf
+++ workload/terraform/jamfpro/buildings.tf
@@ -1,21 +1,24 @@
 
+resource "jamfpro_building" "jamfpro_building_001" {
+  name            = "tf-demo-v2-ONE"
+}
+
+resource "jamfpro_building" "jamfpro_building_002" {
+  name            = "tf-demo-v2-TWO"
+}
 
 resource "jamfpro_building" "jamfpro_building_003" {
-  count = 10
-  name            = "tf-demo-v2-London Wall-${count.index}"
-  street_address1 = "The McIntosh Tree"
-  street_address2 = "One Apple Park Way"
-  city            = "Cupertino"
-  state_province  = "California"
-  zip_postal_code = "95014"
-  country         = "The United States of America"
+  name            = "tf-demo-v2-THREE"
 }
 
+resource "jamfpro_building" "jamfpro_building_004" {
+  name            = "tf-demo-v2-FOUR"
+}
+
+resource "jamfpro_building" "jamfpro_building_005" {
+  name            = "tf-demo-v2-FIVE"
+}
 
 resource "jamfpro_building" "jamfpro_building_006" {
-  name            = "tf-demo-v2-Joseph's second House"
-}
-
-resource "jamfpro_building" "jamfpro_building_007" {
-  name            = "tf-demo-v2-Joseph's First House"
+  name            = "tf-demo-v2-SIX"
 }
