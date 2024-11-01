919a9ace7d16307e92af7b0242eaffcc59ab397a
Joseph Little <joelittle11@live.com>
2024-11-01 10:22:46 +0000

    removed redundant bash branch check script

--- .github/scripts/branch_name_check.sh
+++
@@ -1,6 +0,0 @@
-if [[ $BRANCH_NAME == bugfix-* ]] || [[ $BRANCH_NAME == feat-* ]]; then
-    echo "Branch name '$BRANCH_NAME' matches required pattern (bugfix-* or feat-*)"
-else
-    echo "Branch name '$BRANCH_NAME' does not match required pattern (bugfix-* or feat-*)"
-    exit 1
-fi
