ec69a1ece6fb31aa2cdf6df5234ea27eb0faecb4
Joseph Little <joelittle11@live.com>
2024-11-08 09:17:02 +0000

    feat: added building

--- workload/terraform/jamfpro/categories.tf
+++ workload/terraform/jamfpro/categories.tf
@@ -14,5 +14,10 @@ resource "jamfpro_category" "category_4" {
   name = "Marketing-1 - JL cake Demo V2"
 }
 
+resource "jamfpro_category" "category_5" {
+  name = "Marketing-1 - JL orange juice Demo V2"
+}
+
+
 
 
