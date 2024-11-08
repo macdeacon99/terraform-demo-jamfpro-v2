d03f194a1b3d84a3f72922f73d49b19db6a0a358
Joseph Little <joelittle11@live.com>
2024-11-08 09:17:54 +0000

    oops

--- workload/terraform/jamfpro/categories.tf
+++ workload/terraform/jamfpro/categories.tf
@@ -14,9 +14,6 @@ resource "jamfpro_category" "category_4" {
   name = "Marketing-1 - JL cake Demo V2"
 }
 
-resource "jamfpro_category" "category_5" {
-  name = "Marketing-1 - JL orange juice Demo V2"
-}
 
 
 
