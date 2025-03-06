45da95c84388bc9f7aa91a4ae1673f9bfa153e47
Joseph <joelittle11@live.com>
2025-03-06 11:47:11 +0000

    feat: terraform diff

--- workload/terraform/jamfpro/scripts.tf
+++ workload/terraform/jamfpro/scripts.tf
@@ -29,15 +29,15 @@
 # }
 
 
-# resource "jamfpro_script" "jamfpro_script_4" {
-#   name            = "tf-demo-v2-script-4"
-#   script_contents = "echo 'hello world'"
-#   os_requirements = "13"
-#   priority        = "BEFORE"
-#   info            = "Adds target user or group to specified group membership, or removes said membership."
-#   notes           = "Jamf Pro script parameters: 4"
-#   parameter4      = "/"
-# }
+resource "jamfpro_script" "jamfpro_script_4" {
+  name            = "tf-demo-v2-script-4"
+  script_contents = "echo 'hello world'"
+  os_requirements = "13"
+  priority        = "BEFORE"
+  info            = "Adds target user or group to specified group membership, or removes said membership."
+  notes           = "Jamf Pro script parameters: 4"
+  parameter4      = "/"
+}
 
 # resource "jamfpro_script" "jamfpro_script_5" {
 #   name            = "tf-demo-v2-script-5"
